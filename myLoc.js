e807f7f8007d619e561474b082efebed74c1a079
Sasha15 <alex_brovko@mail.ru>
2018-06-28 16:36:06 +0300

    adding Google maps

--- myLoc.js
+++ myLoc.js
@@ -1,6 +1,7 @@
 window.onload = getMyLocation;
 
 function getMyLocation () {
+	//check if geolocation available
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(displayLocation, displayError);
 	} else  {
@@ -8,15 +9,21 @@ function getMyLocation () {
 	}
 }
 function displayLocation (position) {
+
+	//get current lat and long
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
 
 	var div = document.getElementById("location");
 	div.innerHTML = "You are at Latitude: " + latitude + ", Longitude: " + longitude;
 
+	//get distance from current position to hardcoded position in object ourCoords
 	var km = computeDistance(position.coords, ourCoords);
+
 	var distance = document.getElementById("distance");
 	distance.innerHTML = "You are  " + km + " km from Wickedly Smart HQ";
+
+	showMap(position.coords)
 }
 
 function displayError(error) {
@@ -34,6 +41,7 @@ function displayError(error) {
 	div.innerHTML = errorMessage;
 }
 
+// function for counting distance from current position to hardcoded position in object ourCoords
 function computeDistance(startCoords, destCoords) {
 	startLatRads = degreesToRadians(startCoords.latitude);
 	startLongRads = degreesToRadians(startCoords.longitude);
@@ -41,16 +49,40 @@ function computeDistance(startCoords, destCoords) {
 	endLongRads = degreesToRadians(destCoords.longitude);
 
 	var radius = 6371; // radius of the Earth in km
+
+	// havershine equation - count distance on shere
 	var distance = Math.acos( Math.sin( startLatRads ) * Math.sin(endLatRads)  + Math.cos(startLatRads) * Math.cos(endLatRads) * Math.cos(startLongRads - endLongRads)) * radius;
 	return distance ;
 }
 
+// to convert defrees in radians
 function degreesToRadians(degrees){
 	var radians = (degrees * Math.PI) / 180;
 	return radians;
 }
 
+// Hardcoded object in purpose to count distance
 var ourCoords = {
 	latitude :  47.624851,
 	longitude : -122.52099
+}
+
+//variable which will be holding Google map afer we create it
+var map;
+
+function showMap(coords){
+
+	//LatLng is a consturctor
+	var googleLatAndLong = new google.maps.LatLng(coords.latitude,  coords.longitude);
+	var mapOptions = {
+		zoom : 17,
+		center : googleLatAndLong,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	}
+
+	var mapDiv = document.getElementById("map");
+
+	// Map is constructor from Google API which takes an element
+	// and options and create and returns a map object
+	map = new google.maps.Map(mapDiv, mapOptions);
 }
